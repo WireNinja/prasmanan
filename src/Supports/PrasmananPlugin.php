5abe57cc24c9098ce604bc46be75a3af4652a7da
Yohanes Eka Adhi Setiawan <yohanesekaadhisetiawan@Yohaness-Mac-mini.local>
2026-03-11 18:59:45 +0700

    feat: refactor into Composer Plugin to avoid manual hook scripts

---
+++ src/Supports/PrasmananPlugin.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace WireNinja\Prasmanan\Supports;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+
+class PrasmananPlugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        // No action needed on activation
+    }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+        // No action needed on deactivation
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        // No action needed on uninstall
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ScriptEvents::PRE_AUTOLOAD_DUMP => 'ensureDirectoriesExist',
+            ScriptEvents::PRE_UPDATE_CMD => 'ensureDirectoriesExist',
+            ScriptEvents::PRE_INSTALL_CMD => 'ensureDirectoriesExist',
+        ];
+    }
+
+    public static function ensureDirectoriesExist(Event $event): void
+    {
+        $directories = [
+            'resources/svg',
+            'resources/icons/lucide',
+            'resources/css/filament',
+            'lang/id',
+        ];
+
+        // getcwd() returns the root directory of the project where `composer update` is run
+        $baseDir = getcwd();
+
+        foreach ($directories as $dir) {
+            $path = $baseDir . DIRECTORY_SEPARATOR . $dir;
+
+            if (! is_dir($path)) {
+                if (@mkdir($path, 0755, true)) {
+                    @file_put_contents($path . DIRECTORY_SEPARATOR . '.gitkeep', '');
+                }
+            }
+        }
+    }
+}
