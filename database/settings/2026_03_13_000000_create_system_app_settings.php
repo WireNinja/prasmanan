4021b53b9f091109658a53ce4c12a62346c2b0ea
Yohanes Eka Adhi Setiawan <yohanesekaadhisetiawan@Yohaness-Mac-mini.local>
2026-03-13 00:41:15 +0700

    chore: align migration defaults with class attributes

--- database/settings/2026_03_13_000000_create_system_app_settings.php
+++ database/settings/2026_03_13_000000_create_system_app_settings.php
@@ -9,8 +9,8 @@ return new class extends SettingsMigration
         $this->migrator->add('system_app_settings.brand_name', 'Prasmanan');
         $this->migrator->add('system_app_settings.brand_logo', null);
         $this->migrator->add('system_app_settings.is_dark_mode_enabled', true);
-        $this->migrator->add('system_app_settings.custom_font', 'Inter');
-        $this->migrator->add('system_app_settings.sidebar_width', '16rem');
+        $this->migrator->add('system_app_settings.custom_font', 'IBM Plex Sans');
+        $this->migrator->add('system_app_settings.sidebar_width', '350px');
         $this->migrator->add('system_app_settings.is_sidebar_collapsible_on_desktop', true);
         $this->migrator->add('system_app_settings.are_navigation_groups_collapsible', true);
     }
